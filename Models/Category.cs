0aad475c0a126dded0d34f112e53a4f919dd1e99
Svetoslav <speednet@mail.bg>
2022-07-13 08:48:24 +0300

    FirstCommit

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Invalid Data")]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
